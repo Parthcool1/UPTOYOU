3ffd1b6b36738d9d527a825ad2c3aa4b9d495e48
Parthcool1 <47765541+Parthcool1@users.noreply.github.com>
2019-03-17 22:05:06 -0400

    Create Index.js

---
+++ Index.js
@@ -0,0 +1,16 @@
+("#play").click(()=>{
+  $("#home").hide();
+  $("#game").show();
+});
+
+function Game () {
+  this.Box = function (text) {
+    this.text = text;
+    $('#box').text(text);
+  }
+}
+
+var game = new Game();
+
+var box = new game.Box('These are the instructions');
+var box = new game.Box('WASD to move around');
